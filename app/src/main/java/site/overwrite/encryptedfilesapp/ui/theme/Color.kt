753d58de31fbb9f98d5093c2af726689cadca1d5
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2023-12-23 14:07:41 +0800

    Changed theme colours

--- app/src/main/java/site/overwrite/encryptedfilesapp/ui/theme/Color.kt
+++ app/src/main/java/site/overwrite/encryptedfilesapp/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package site.overwrite.encryptedfilesapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Indigo80 = Color(0xFFBBC3FF)
+val IndigoGrey60 = Color(0xFF8F8F9F)
+val Pink60 = Color(0xFFA5889B)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Indigo40 = Color(0xFF3E54C4)
+val IndigoGrey40 = Color(0xFF595D78)
+val Pink40 = Color(0xFF7C5072)
--- app/src/main/java/site/overwrite/encryptedfilesapp/ui/theme/Theme.kt
+++ app/src/main/java/site/overwrite/encryptedfilesapp/ui/theme/Theme.kt
@@ -16,14 +16,14 @@ import androidx.compose.ui.platform.LocalView
 import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = Indigo80,
+    secondary = IndigoGrey60,
+    tertiary = Pink60
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
+    primary = Indigo40,
+    secondary = IndigoGrey40,
     tertiary = Pink40
 
     /* Other default colors to override
