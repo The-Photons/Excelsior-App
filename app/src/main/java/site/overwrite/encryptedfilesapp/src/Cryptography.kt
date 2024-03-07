87d9acf41c338c57fe6ca93ad21925cc1d861b8e
PhotonicGluon <25820201+PhotonicGluon@users.noreply.github.com>
2024-03-07 20:48:26 +0800

    Change copyright text
    Changed copyright text to use PhotonicGluon

--- app/src/main/java/site/overwrite/encryptedfilesapp/src/Cryptography.kt
+++ app/src/main/java/site/overwrite/encryptedfilesapp/src/Cryptography.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2023 Kan Onn Kit.
+ * Copyright (c) 2023 PhotonicGluon.
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
